d93d07b1607cbb8cb0724e5cddd7bc095ac07f1a
esther-v <esvalas@gmail.com>
2021-09-28 15:15:22 +0200

    add some style

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const storeProducts = document.querySelectorAll('.store-product');
 for (i = 0; i < btns.length; i ++) {
     btns[i].addEventListener('click', (e) => {
         e.preventDefault();
-
+        
         const filter = e.target.dataset.filter;
 
         storeProducts.forEach((product) => {
@@ -15,6 +15,7 @@ for (i = 0; i < btns.length; i ++) {
             } else {
                 if (product.classList.contains(filter)) {
                     product.style.display = "block";
+    
                 } else {
                     product.style.display = "none"
                 }
