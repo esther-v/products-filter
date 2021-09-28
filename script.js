6d902cd6d180bd65402d6973f0e5274d1b76822a
esther-v <esvalas@gmail.com>
2021-09-28 14:11:49 +0200

    js ok

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btns = document.querySelectorAll('.btn');
 const storeProducts = document.querySelectorAll('.store-product');
 
+
+//DISPLAYING PRODUCTS BY category
 for (i = 0; i < btns.length; i ++) {
     btns[i].addEventListener('click', (e) => {
         e.preventDefault();
@@ -14,9 +16,27 @@ for (i = 0; i < btns.length; i ++) {
                 if (product.classList.contains(filter)) {
                     product.style.display = "block";
                 } else {
-                    product.style.display = "none";
+                    product.style.display = "none"
                 }
             }
         })
     })
 }
+
+// SEARCH FILTER
+const search = document.getElementById("search");
+
+search.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    const searchValue = search.value.toLowerCase().trim();
+
+    for (i = 0; i < storeProducts.length; i++) {
+        if (storeProducts[i].classList.contains(searchValue)) {
+            storeProducts[i].style.display = "block";
+        } else if (searchValue == "") {
+            storeProducts[i].style.display = "block";
+        } else {
+            storeProducts[i].style.display = "none";
+        }
+    }
+})
